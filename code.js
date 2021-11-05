b2f091dc59e08857e1a877573086cb04247fea03
yikuansun <yikuansun2015@gmail.com>
2021-11-04 22:52:32 -0400

    Make a bug

--- code.js
+++ code.js
@@ -9,3 +9,9 @@ vignette_layer.costume = "graphics/vignette.svg";
 vignette_layer.layer = 696969;
 vignette_layer.style.mixBlendMode = "multiply";
 canvas.addsprite(vignette_layer);
+
+var playable = new daize.sprite(10, 10, 50, 50, 0, unit_type);
+playable.costume = "graphics/bug1.svg";
+canvas.addsprite(playable);
+playable.style.filter = "drop-shadow(0 0 0.5vh #00AAFF) drop-shadow(0 0 2vh #00AAFF)";
+playable.style.mixBlendMode = "screen";

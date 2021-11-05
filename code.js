f6dda469120fb28400cbca802e66a80f5e3e3a95
yikuansun <yikuansun2015@gmail.com>
2021-11-04 22:37:07 -0400

    Background color

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
 const unit_type = "vh";
 var canvas = new daize.displayCanvas(160, 100, unit_type);
 document.querySelector("center").appendChild(canvas);
+
+canvas.style.backgroundColor = "#111111";

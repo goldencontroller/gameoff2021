7720e2800c37bea7937465337248baad852eaa39
yikuansun <yikuansun2015@gmail.com>
2021-11-04 22:46:56 -0400

    Add vignette

--- code.js
+++ code.js
@@ -3,3 +3,9 @@ var canvas = new daize.displayCanvas(160, 100, unit_type);
 document.querySelector("center").appendChild(canvas);
 
 canvas.style.backgroundColor = "#111111";
+
+var vignette_layer = new daize.sprite(160, 100, 80, 50, 0, unit_type);
+vignette_layer.costume = "graphics/vignette.svg";
+vignette_layer.layer = 696969;
+vignette_layer.style.mixBlendMode = "multiply";
+canvas.addsprite(vignette_layer);

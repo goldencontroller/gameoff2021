bec21f368a9a5f642696e7eb76abf35ce6cb7a1a
yikuansun <yikuansun2015@gmail.com>
2021-11-04 22:57:50 -0400

    Begin building main game loop

--- code.js
+++ code.js
@@ -15,3 +15,16 @@ playable.costume = "graphics/bug1.svg";
 canvas.addsprite(playable);
 playable.style.filter = "drop-shadow(0 0 0.5vh #00AAFF) drop-shadow(0 0 2vh #00AAFF)";
 playable.style.mixBlendMode = "screen";
+
+var keysDown = {};
+window.addEventListener("keydown", function(e) { keysDown[e.key] = true; });
+window.addEventListener("keyup", function(e) { keysDown[e.key] = false; });
+
+function mainGameLoop() {
+    if (keysDown.ArrowUp) {
+        playable.y -= 0.69;
+    }
+    requestAnimationFrame(mainGameLoop);
+}
+
+mainGameLoop();
